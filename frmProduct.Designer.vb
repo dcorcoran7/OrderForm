4ddc9390dc385581825d571416ee2de650b2446a
David Corcoran <142289167+dcorcoran7@users.noreply.github.com>
2023-08-22 21:12:11 -0400

    Add files via upload

---
+++ frmProduct.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmProduct
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDateTime = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cboProduct = New System.Windows.Forms.ComboBox()
+        Me.btnOrder = New System.Windows.Forms.Button()
+        Me.lblOrder = New System.Windows.Forms.Label()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.lstOrders = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.tmrNow = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Current day/time"
+        '
+        'txtDateTime
+        '
+        Me.txtDateTime.Enabled = False
+        Me.txtDateTime.Location = New System.Drawing.Point(188, 38)
+        Me.txtDateTime.Name = "txtDateTime"
+        Me.txtDateTime.Size = New System.Drawing.Size(366, 26)
+        Me.txtDateTime.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(52, 128)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Product"
+        '
+        'cboProduct
+        '
+        Me.cboProduct.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboProduct.FormattingEnabled = True
+        Me.cboProduct.Location = New System.Drawing.Point(188, 128)
+        Me.cboProduct.Name = "cboProduct"
+        Me.cboProduct.Size = New System.Drawing.Size(366, 28)
+        Me.cboProduct.TabIndex = 4
+        '
+        'btnOrder
+        '
+        Me.btnOrder.Location = New System.Drawing.Point(584, 118)
+        Me.btnOrder.Name = "btnOrder"
+        Me.btnOrder.Size = New System.Drawing.Size(87, 46)
+        Me.btnOrder.TabIndex = 5
+        Me.btnOrder.Text = "Order"
+        Me.btnOrder.UseVisualStyleBackColor = True
+        '
+        'lblOrder
+        '
+        Me.lblOrder.BackColor = System.Drawing.Color.White
+        Me.lblOrder.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblOrder.Location = New System.Drawing.Point(188, 228)
+        Me.lblOrder.Name = "lblOrder"
+        Me.lblOrder.Size = New System.Drawing.Size(365, 185)
+        Me.lblOrder.TabIndex = 7
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(584, 443)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(87, 46)
+        Me.btnClose.TabIndex = 8
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'lstOrders
+        '
+        Me.lstOrders.FormattingEnabled = True
+        Me.lstOrders.ItemHeight = 20
+        Me.lstOrders.Location = New System.Drawing.Point(45, 228)
+        Me.lstOrders.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.lstOrders.Name = "lstOrders"
+        Me.lstOrders.Size = New System.Drawing.Size(121, 184)
+        Me.lstOrders.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(52, 186)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 20)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Orders"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(183, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(120, 20)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Order Summary"
+        '
+        'tmrNow
+        '
+        Me.tmrNow.Enabled = True
+        '
+        'frmProduct
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(706, 526)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lstOrders)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.lblOrder)
+        Me.Controls.Add(Me.btnOrder)
+        Me.Controls.Add(Me.cboProduct)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtDateTime)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmProduct"
+        Me.Text = "Product"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDateTime As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cboProduct As ComboBox
+    Friend WithEvents btnOrder As Button
+    Friend WithEvents lblOrder As Label
+    Friend WithEvents btnClose As Button
+    Friend WithEvents lstOrders As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents tmrNow As Timer
+End Class
